b6219b77bdd7c44aaf67079a783927af960b1697
dkratofil <dkratofil@etfos.hr>
2020-07-09 17:54:59 +0200

    Add postcss config and run basic VolumeViewer example

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var vtkRules = require('vtk.js/Utilities/config/dependency.js').webpack.core.rules;
 
 // Optional if you want to load *.css and *.module.css files
-// var cssRules = require('vtk.js/Utilities/config/dependency.js').webpack.css.rules;
+var cssRules = require('vtk.js/Utilities/config/dependency.js').webpack.css.rules;
 
 var entry = path.join(__dirname, './src/index.js');
 const sourcePath = path.join(__dirname, './src');
@@ -18,6 +18,49 @@ module.exports = {
   module: {
     rules: [
         { test: /\.html$/, loader: 'html-loader' },
+        // {
+        //   test: /\.css$/i,
+        //   use: ['style-loader', 'css-loader'],
+        // },
+        {
+          test: /\.module\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[name]-[local]_[sha512:hash:base64:5]',
+                },
+              },
+            },
+            { loader: 'postcss-loader' },
+          ],
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          use: [
+            'url-loader?limit=10000',
+            'img-loader'
+          ],
+        },
+          
+        
+        // {
+        //   test: /\.(png|svg|jpe?g|gif)$/,
+        //   include: /images/,
+        //   use: [
+        //     {
+        //       loader: 'file-loader',
+        //       options: {
+        //         name: '[name].[ext]',
+        //         outputPath: 'images/',
+        //         publicPath: 'images/'
+        //       }
+        //     }
+        //   ]
+        // },
+        
     ].concat(vtkRules),
   },
   resolve: {
@@ -27,3 +70,14 @@ module.exports = {
     ],
   },
 };
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+// };
